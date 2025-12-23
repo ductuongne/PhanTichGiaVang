c4866f3fca0e29d0cc053e1112e05d55cab6316e
cpngMaika <phongutevippro@gmail.com>
2025-12-23 14:48:51 +0700

    commit cua phong

--- pages/BieuDoGia.py
+++ pages/BieuDoGia.py
@@ -1,19 +1,53 @@
 import streamlit as st
+import pandas as pd
 from components.navbar import render_navbar
 from components.clean_ui import clean_streamlit_ui
-from utils.data_loader import load_pnj_history
-from utils.plot import plot_gold_pnj
+from utils.plot import plot_gold_simple
 
 st.set_page_config(page_title="Biểu đồ giá", layout="wide")
 render_navbar()
 
-df = load_pnj_history()
 
-branch = st.selectbox(
-    "Chọn chi nhánh",
-    df["BranchName"].unique()
-)
+col1, col2 = st.columns(2)
 
-plot_gold_pnj(df, branch)
+with col1:
+    company = st.radio(
+        "Doanh nghiệp",
+        ["PNJ", "SJC"],
+        horizontal=True
+    )
+
+with col2:
+    data_type = st.radio(
+        "Dữ liệu",
+        ["Realtime", "History"],
+        horizontal=True
+    )
+
+# ==== map file ====
+FILE_MAP = {
+    ("PNJ", "History"): "data/pnj/gold_pnj_history.csv",
+    ("PNJ", "Realtime"): "data/pnj/gold_pnj_realtime.csv",
+    ("SJC", "History"): "data/sjc/gold_sjc_history.csv",
+    ("SJC", "Realtime"): "data/sjc/gold_sjc_realtime.csv",
+}
+
+file_path = FILE_MAP[(company, data_type)]
+
+df = pd.read_csv(file_path)
+# ==== filter branch (History only) ====
+if data_type == "History":
+    branches = df["BranchName"].unique()
+    selected_branch = st.selectbox(
+        "Chi nhánh",
+        branches
+    )
+
+    df = df[df["BranchName"] == selected_branch]
+
+
+
+# ==== render chart ====
+plot_gold_simple(df, company, data_type)
 
 clean_streamlit_ui()
--- utils/data_loader.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-def load_pnj_history():
-    df = pd.read_csv("data/pnj/gold_pnj_history.csv")
-    df["date"] = pd.to_datetime(df["date"])
-    return df
