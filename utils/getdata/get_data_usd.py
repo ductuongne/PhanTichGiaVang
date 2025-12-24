b0c638462abd8ee76220396e3c1e76869bc1f6cc
ductuongne <tuongnoob432@gmail.com>
2025-12-24 21:15:43 +0700

    usd realtime fix mot so loi

--- utils/getdata/get_data_usd.py
+++ utils/getdata/get_data_usd.py
@@ -2,8 +2,9 @@ import requests
 import os
 import pandas as pd
 
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 CSV_PATH_USD = os.path.join(BASE_DIR, "data", "usd", "usd_vnd_rate.csv")
+
 CURRENTCY_KEY = 'cur_live_oLAQPXeWILkKt1Mvd4ImEigkUraypXPrn5r2IV4g'
 def get_data_usd():
     url = "https://api.currencyapi.com/v3/latest"
@@ -20,7 +21,8 @@ def get_data_usd():
     date = response.json()['meta']['last_updated_at']
     date = pd.to_datetime(date)
     df = pd.DataFrame({'date': [date.date()], 'usd_vnd_rate': [usd_vnd_rate]})
-    df.to_csv(CSV_PATH_USD, mode='w', header=False, index=False)
+    os.makedirs(os.path.dirname(CSV_PATH_USD), exist_ok=True)
+    df.to_csv(CSV_PATH_USD, mode='w', header=True, index=False)
     return usd_vnd_rate
     
 if __name__ == "__main__":
